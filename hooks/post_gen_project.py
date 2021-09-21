bbdfca024235abad372d5a271b99fcf04aac0637
Queensferry <queensferry.me@gmail.com>
2021-09-21 14:39:06 +0800

    feat: integrate `nitpick` (https://github.com/RSSerpent/RSSerpent/issues/24)

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,5 +5,5 @@ import subprocess
 
 subprocess.run(["git", "init"])
 subprocess.run(["poetry", "install"])
-subprocess.run(["poetry", "run", "pre-commit", "install", "--install-hooks", "-t", "pre-commit", "-t", "commit-msg"])
+subprocess.run(["poetry", "run", "pre-commit", "install", "-t", "pre-commit", "-t", "commit-msg"])
 subprocess.run(["poetry", "run", "pre-commit", "autoupdate"])
