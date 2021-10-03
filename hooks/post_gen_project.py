a56fe30ebe2c2dda4c1a2a2b6e57e4cadda50cd5
Queensferry <queensferry.me@gmail.com>
2021-10-03 23:33:27 +0800

    feat: don't install deps in tmpdir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+import os
 import subprocess
+from tempfile import gettempdir
 
 
 subprocess.run(["git", "init"])
-subprocess.run(["poetry", "install"])
-subprocess.run(["poetry", "run", "pre-commit", "install", "-t", "pre-commit", "-t", "commit-msg"])
-subprocess.run(["poetry", "run", "pre-commit", "autoupdate"])
+if not os.getcwd().startswith(os.path.realpath(gettempdir())):
+    subprocess.run(["poetry", "install"])
+    subprocess.run(["poetry", "run", "pre-commit", "install", "-t", "pre-commit", "-t", "commit-msg"])
+    subprocess.run(["poetry", "run", "pre-commit", "autoupdate"])
